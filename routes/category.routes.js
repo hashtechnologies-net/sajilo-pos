01d561e6942eeba94e301c39f3d2fd62dd33b9af
Sanu33 <56817684+johndoeesq@users.noreply.github.com>
2021-07-30 21:14:36 +0545

    Get route handled

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -5,16 +5,20 @@ const express = require('express');
 const categoryController = require('../controller/category.controller');
 
 const Admin = require('../models/admin.model');
+const Category = require('../models/category.model');
 const allqueryresults = require('../middleware/allqueryresults');
 const authprotect = require('../middleware/auth');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(categoryController.getAllCategory).post(
-	authprotect.protect,
-	//authprotect.authorize('Admin'),
-	categoryController.createCategory
-);
+router
+	.route('/')
+	.get(allqueryresults(Category), categoryController.getAllCategory)
+	.post(
+		authprotect.protect,
+		//authprotect.authorize('Admin'),
+		categoryController.createCategory
+	);
 
 router
 	.route('/:id')
