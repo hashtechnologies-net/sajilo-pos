fdcf787f891b9c06f05f6d01012db4c098985fdb
Sanu33 <56817684+johndoeesq@users.noreply.github.com>
2021-11-17 17:16:35 +0545

    Product Model Modified

--- models/product.models.js
+++ models/product.models.js
@@ -1,52 +1,53 @@
 /** @format */
 
 const mongoose = require('mongoose');
-const ProductSchema = new mongoose.Schema({
-	product_name: {
-		type: String,
-		required: [true, 'Please add a name'],
-		trim: true,
-		maxlength: [50, 'Name cannot be more than of 50 characters'],
-	},
-	product_code: {
-		type: String,
-		required: [true, 'Please enter the product code'],
-		unique: true,
-	},
-	unit_id: {
-		type: mongoose.Schema.ObjectId,
-		ref: 'units',
-		required: true,
-	},
+const ProductSchema = new mongoose.Schema(
+	{
+		product_name: {
+			type: String,
+			required: [true, 'Please add a name'],
+			trim: true,
+			maxlength: [50, 'Name cannot be more than of 50 characters'],
+		},
+		product_code: {
+			type: String,
+			required: [true, 'Please enter the product code'],
+			unique: true,
+		},
+		unit_id: {
+			type: mongoose.Schema.ObjectId,
+			ref: 'units',
+			required: true,
+		},
 
-	unit_price: {
-		type: Number,
-		required: true,
+		unit_price: {
+			type: Number,
+			required: true,
+		},
+		product_status: {
+			type: Boolean,
+			default: false,
+		},
+		description: {
+			type: String,
+		},
+		QR_link: {
+			type: String,
+			default: 'no_img.jpg',
+		},
+		category_id: {
+			type: mongoose.Schema.ObjectId,
+			ref: 'category',
+			required: true,
+		},
+		created_by: {
+			type: mongoose.Schema.ObjectId,
+			required: true,
+		},
 	},
-	product_status: {
-		type: Boolean,
-		default: false,
+	{
+		timestamps: true,
 	},
-	description: {
-		type: String,
-	},
-	QR_link: {
-		type: String,
-		default: 'no_img.jpg',
-	},
-	category_id: {
-		type: mongoose.Schema.ObjectId,
-		ref: 'category',
-		required: true,
-	},
-	created_by: {
-		type: mongoose.Schema.ObjectId,
-		required: true,
-	},
-	created_at: {
-		type: Date,
-		default: Date.now,
-	},
-});
+);
 
 module.exports = mongoose.model('product', ProductSchema);
